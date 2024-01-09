34646e703a62724cf8983bd9d6568e08e0635040
NekuaTicoz <jeanguideau@hotmail.fr>
2024-01-09 15:38:59 +0100

    feat : création page gestionSéance et ajout header dans cette page

---
+++ private/resources/views/gestionSéance.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>Gestion de la séance</title>
+</head>
+<body>
+    
+<?php include "header.blade.php" ?>
+
+    <main>
+
+        
+    </main>
+
+</body>
+</html>
