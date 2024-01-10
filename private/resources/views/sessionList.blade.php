01b8ed98eb164f9c0a03da52887410bfc6a43b69
Sirmetana <lukas.siopathis@gmail.com>
2024-01-10 10:30:14 +0100

    adjustments

--- private/resources/views/sessionList.blade.php
+++ private/resources/views/sessionList.blade.php
@@ -49,12 +49,10 @@
                         ->where('PLON_DATE','>=', $fDay)
                         ->where('PLON_DATE','<', $lDay)
                         ->get();*/
-            echo 'Fday : '.$fDay->format('d-m-Y H:i').'   lDay : '.$lDay->format('Y-m-d H:i:s');
+            //echo 'Fday : '.$fDay->format('d-m-Y H:i').'   lDay : '.$lDay->format('Y-m-d H:i:s');
             $result = DB::Select(
-                //doesn't return anything with this query
                 "SELECT PLON_DATE FROM PLONGEE WHERE PLON_DATE >= ? AND PLON_DATE < ?",
                 [$fDay, $lDay]
-                /*"SELECT PLON_DATE FROM PLONGEE"*/
             );
             foreach($result as $line)
             {
@@ -110,34 +108,6 @@
             <?php
                 getMonthlySessions(10);
             ?>
-        </div>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when a
-        n unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of 
-        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-        software like Aldus PageMaker including versions of Lorem Ipsum. It is a long 
-        established fact that a reader will be distracted by the readable content of a page
-        when looking at its layout. The point of using Lorem Ipsum is that it has a 
-        more-or-less normal distribution of letters, as opposed to using 'Content here, 
-        content here', making it look like readable English. Many desktop publishing packages
-        and web page editors now use Lorem Ipsum as their default model text, and a search 
-        for 'lorem ipsum' will uncover many web sites still in their infancy.
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when a
-        n unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of 
-        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-        software like Aldus PageMaker including versions of Lorem Ipsum. It is a long 
-        established fact that a reader will be distracted by the readable content of a page
-        when looking at its layout. The point of using Lorem Ipsum is that it has a 
-        more-or-less normal distribution of letters, as opposed to using 'Content here, 
-        content here', making it look like readable English. Many desktop publishing packages
-        and web page editors now use Lorem Ipsum as their default model text, and a search 
-        for 'lorem ipsum' will uncover many web sites still in their infancy.
-    </div>
 </div>
 
 </body>
