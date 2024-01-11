2d2b067a5c46a12daa0beb14420f001338c49b91
Louis <louis.piel@etu.unicaen.fr>
2024-01-11 08:34:01 +0100

    feat : supression use en trop

--- private/routes/web.php
+++ private/routes/web.php
@@ -4,22 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConnexionController;
 use App\Http\Controllers\DeconnexionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Responsable;
 
-use App\Http\Controllers\ConnexionController;
 use app\Http\Controllers\PlongeeController;
 use App\Http\Controllers\SessionManager;
 use App\Http\Controllers\sessionListController;
