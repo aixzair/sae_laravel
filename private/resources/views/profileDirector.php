711e14734a3dc1c5302964eb4640e0a0d4e33704
Maxime <maxime.jobard@etu.unicaen.fr>
2024-01-11 13:11:46 +0100

    refactor : petite modif #4.13

--- private/resources/views/profileDirector.php
+++ private/resources/views/profileDirector.php
@@ -7,9 +7,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../../public/css/style.css" />
-  {# <link rel="stylesheet" href="{{asset('css/style.css')}}" /> #}
-  <title>Document</title>
+  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+  <title>Profil</title>
 </head>
 <body>
 
--- private/resources/views/session/directorList.php
+++ private/resources/views/session/directorList.php
@@ -13,20 +13,20 @@ use app\Http\Controllers\PlongeeController;
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../../public/css/style.css" />
-  <!--<link rel="stylesheet" href="{{asset('css/style.css')}}" />-->
-  <title>Document</title>
+  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+  <title>Mes Plongées</title>
 </head>
 <body>
+    @include('header');
     <main>
         <div>
             <?php
             $result = DB::Select("
-            Select plon_date, plon_debut, plon_fin 
-            from PLONGEE 
-            join ADHERENT on plon_directeur = ad_email 
-            WHERE ad_email = $_SESSION[email]
-            ");
+                Select plon_date, plon_debut, plon_fin 
+                from PLONGEE 
+                join ADHERENT on plon_directeur = ad_email 
+                WHERE ad_email = ".session('email')
+            );
             ?>
             @foreach($result as $ligne)
                 <div>
