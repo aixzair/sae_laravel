8b76887459c383ba128a95894a5ea3dbd1207dd2
emeline <emeline.laigle@unicaen.fr>
2024-01-12 01:38:09 +0100

    docs: ajout de commentaires à Palanque (le model) #5.1

--- private/app/Models/Palanque.php
+++ private/app/Models/Palanque.php
@@ -10,10 +10,14 @@ class Palanque extends Model
 {
     use HasFactory;
 
+    // Define the table associated with the model
     protected $table = 'palanque';
 
+    // Specify the fillable attributes for mass assignment
     protected $fillable = ['pal_id', 'sea_id', 'pal_effectifs', 'pal_heure_debut', 'pal_heure_fin', 'pal_prondeur_max', 'plon_date'];
 	
+
+    // Disable automatic timestamps for this model
 	public $timestamps = false;
 }
 
